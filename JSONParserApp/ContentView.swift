3e20a136134e28ea4528f69c29308f1ba1a4fb05
Gonzalo Gamez <takosaga@gmail.com>
2020-04-08 13:51:35 -0500

    Initial Commit

---
+++ JSONParserApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONParserApp
+//
+//  Created by Gonzalo Gamez on 4/8/20.
+//  Copyright © 2020 Gamez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
